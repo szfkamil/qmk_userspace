86b9d8eaaa6fc00a81574311536bec3a4f2529d3
Kamil Zwoinski <main@kamilzwoinski.com>
2026-09-04 09:44:02 +0200

    include bk_pointing_device module to configure auto-mouse and auto-precision

--- keyboards/bastardkb/charybdis/3x5/keymaps/szfkamil/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/szfkamil/keymap.c
@@ -14,6 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
+#include "bk_pointing_device.h"
 
 #ifdef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
 #    include "timer.h"
