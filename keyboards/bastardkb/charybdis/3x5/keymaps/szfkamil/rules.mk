67e96776c0fcf9d66d274ab82368d74bfb1cbe7b
Kamil Zwoinski <main@kamilzwoinski.com>
2026-09-12 08:52:10 +0200

    Add extra rules, includling LTO to optimize firmware size

--- keyboards/bastardkb/charybdis/3x5/keymaps/szfkamil/rules.mk
+++ keyboards/bastardkb/charybdis/3x5/keymaps/szfkamil/rules.mk
@@ -4,3 +4,7 @@ RGBLIGHT_ENABLE = no
 RGB_MATRIX_ENABLE = yes
 COMBO_ENABLE = yes
 TRI_LAYER_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+# LTO_ENABLE is a configuration option in QMK that enables Link Time Optimization, 
+# which can help reduce the size of the firmware by optimizing the code during the linking stage.
+LTO_ENABLE = yes
